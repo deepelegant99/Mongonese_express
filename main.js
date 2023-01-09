784cd3f773e26ab82e45a1e0acb8132e9976d7b7
phillip wong <deepelegant99@gmail.com>
2023-01-08 20:24:19 -0800

    Co-authored-by: sonkyung <sonkyung@users.noreply.github.com> Co-authored-by: crossh2001 <crossh2001@users.noreply.github.com>

--- main.js
+++ main.js
@@ -12,14 +12,36 @@ app.get("/dogs", async (req, res) => {
   return res.status(200).json(allDogs);
 });
 
+app.get('/dogs/:id', async (req, res)=>{
+  const {id} = req.params;
+  const dog = Dog.findById(id)
+  return res.status(200).json(dog)
+})
+app.post('/dogs', async (req, res)=>{
+  const newDog = new Dog({...req.body});
+  const insertedDog = await newDog.save();
+  return res.status(201).json(insertedDog);
+})
 
+app.put('/dogs/:id', async (req, res)=>{
+  const {id} = req.params;
+  await Dog.updateOne({id}, req.body)
+  const updatedDog = await Dog.findById(id)
+  return res.status(200).json(updatedDog)
+})
+
+app.delete('/dogs/:id', async (req, res)=>{
+  const {id} = req.params;
+  const deletedDog = await Dog.findByIdAndDelete(id)
+  return res.status(200).json(deletedDog)
+})
 
 const start = async () => {
   try {
     await mongoose.connect(process.env.Database_URI);
     app.listen(PORT, () => console.log(`Server is listening in port ${PORT}`));
   } catch (err) {
-    console.log(err);
+   // console.log(err);
   }
 };
 
